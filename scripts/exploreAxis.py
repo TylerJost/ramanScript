e1484d692ec31a48e44a22951e3092827020e072
Tyler Jost <tyler_jost@utexas.edu>
2022-11-19 16:39:37 -0600

    Cleaned data, made intensities

--- scripts/exploreAxis.py
+++ scripts/exploreAxis.py
@@ -4,5 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 # %%
 experiment = 'esamInit'
-spectras = np.load(f'../data/{experiment}.npy', allow_pickle=True)
+scans = list(np.load(f'../data/{experiment}.npy', allow_pickle=True))
+
+with open('../data/RamanAxisforMCR.txt') as f:
+    axisInfo = f.read()
+
+axisInfo = [float(num) for num in axisInfo.split('\n')[1:-1]]
+# Remove non-correct scan
+for i, scan in enumerate(scans):
+    if len(scan.spectra[0]) != len(axisInfo):
+        scans.pop(i)
+    # For now also remove non-square images 
+    if int(np.sqrt(len(scan.spectra))) != np.sqrt(len(scan.spectra)):
+        scans.pop(i)
+# %% Plot image
+scan = scans[3]
+for i in range(100):
+    plt.plot(scan.spectra[i])
+    plt.title(i)
+    plt.show()
 # %%
+intensities = []
+for spectra in scan.spectra:
+    intensities.append(sum(spectra[480:600]))
+plt.imshow(np.array(intensities).reshape((100,100)), cmap='gray')
