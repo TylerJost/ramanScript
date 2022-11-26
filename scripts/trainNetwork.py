221c5642f000b968f4f2ebfcc8259e57ccc6dedf
Tyler Jost <tyler_jost@utexas.edu>
2022-11-26 12:05:56 -0600

    changed hyperparameter lcoations

--- scripts/trainNetwork.py
+++ scripts/trainNetwork.py
@@ -4,6 +4,7 @@ This is the neural network to load and train a neural network on time series cla
 """
 # %%
 from ramanScript import ramanSpectra, loadSpectralData, splitDataBalanced
+from resnet1d import ResNet1D, MyDataset
 
 import numpy as np
 import pickle
@@ -12,22 +13,22 @@ from tqdm import tqdm
 from matplotlib import pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix
 
-from resnet1d import ResNet1D, MyDataset
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+
+# %matplotlib inline
 # %%
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
 
-# Hyper-parameters
-num_epochs = 50
-batch_size = 32
-
+batch_size=32
 # Get data
-allSpectra, phenotypes = loadSpectralData(experiment='esamInit')
+experiment = 'esamInit'
+allSpectra, phenotypes = loadSpectralData(experiment=experiment)
 X_train, X_test, y_train, y_test = splitDataBalanced(allSpectra, phenotypes)
 print(X_train.shape, y_train.shape)
 train_dataset = MyDataset(X_train, y_train)
@@ -38,8 +39,11 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, drop_last=False)
 # %%
 dataiter = iter(train_loader)
 spectra, labels = dataiter.next()
+print(spectra.shape)
 # %%
-# make model
+# Hyper-parameters
+num_epochs = 50
+# Model parameters
 device_str = "cuda"
 device = torch.device(device_str if torch.cuda.is_available() else "cpu")
 kernel_size = 16
@@ -54,7 +58,7 @@ model = ResNet1D(
     stride=stride, 
     groups=32, 
     n_block=n_block, 
-    n_classes=4, 
+    n_classes=2, 
     downsample_gap=downsample_gap, 
     increasefilter_gap=increasefilter_gap, 
     use_do=True)
@@ -66,10 +70,11 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
 criterion = torch.nn.CrossEntropyLoss()
 # %%
+allLoss = []
 for epoch in tqdm(range(num_epochs), desc=f"epoch", leave=False):
     # train
     model.train()
-    for i, batch in enumerate(tqdm(train_loader, desc=f"spectra", leave=False)):
+    for i, batch in enumerate(tqdm(train_loader, desc=f"spectra", position=0, leave=True)):
 
         spectra, labels = tuple(t.to(device) for t in batch)
         # spectra = spectra.to(device)
@@ -81,4 +86,11 @@ for epoch in tqdm(range(num_epochs), desc=f"epoch", leave=False):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        allLoss.append(float(loss.cpu().detach().numpy()))
     scheduler.step(loss)
+
+# %%
+# plt.plot(allLoss)
+
+# %%
+torch.save(model.state_dict(), f'../models/{experiment}Final.pth')
