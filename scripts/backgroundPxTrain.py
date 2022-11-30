ed9c4b6464b726d55699c555ead9b74bf60013da
Tyler Jost <tyler_jost@utexas.edu>
2022-11-30 15:35:09 -0600

    init commit for background training

---
+++ scripts/backgroundPxTrain.py
@@ -0,0 +1,6 @@
+# %% [markdown]
+"""
+This is a script to train a neural network on background pixels. If the network is
+able to predict on background alone it may signal that it is the 
+"""
+# %%
