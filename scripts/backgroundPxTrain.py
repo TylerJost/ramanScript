ffc79e9356462cf1b7d23f213524ea56279544ac
Tyler Jost <tyler_jost@utexas.edu>
2022-12-01 14:27:42 -0600

    fixed merge error?

--- scripts/backgroundPxTrain.py
+++ scripts/backgroundPxTrain.py
@@ -121,7 +121,6 @@ y_test = phenotypes[testIdx].astype('int')
 # %%
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-<<<<<<< HEAD
 
 batch_size=32
 print(X_train.shape, y_train.shape)
@@ -225,72 +224,3 @@ plt.title(f'AUC = {roc_auc:0.3f}')
 plt.savefig('../figures/rocCurveBackground.png', dpi=600)
 plt.show()
 
-=======
-
-batch_size=32
-print(X_train.shape, y_train.shape)
-train_dataset = MyDataset(X_train, y_train)
-test_dataset = MyDataset(X_test, y_test)
-train_loader = DataLoader(train_dataset, batch_size=batch_size)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, drop_last=False)
-
-# %%
-dataiter = iter(train_loader)
-spectra, labels = dataiter.next()
-print(spectra.shape)
-# %%
-# Hyper-parameters
-num_epochs = 50
-# Model parameters
-device_str = "cuda"
-device = torch.device(device_str if torch.cuda.is_available() else "cpu")
-kernel_size = 16
-stride = 2
-n_block = 48
-downsample_gap = 6
-increasefilter_gap = 12
-model = ResNet1D(
-    in_channels=1, 
-    base_filters=128, # 64 for ResNet1D, 352 for ResNeXt1D
-    kernel_size=kernel_size, 
-    stride=stride, 
-    groups=32, 
-    n_block=n_block, 
-    n_classes=2, 
-    downsample_gap=downsample_gap, 
-    increasefilter_gap=increasefilter_gap, 
-    use_do=True)
-model.to(device)
-
-# train and test
-model.verbose = False
-optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
-criterion = torch.nn.CrossEntropyLoss()
-# %%
-allLoss = []
-for epoch in tqdm(range(num_epochs), desc=f"epoch", leave=False):
-    # train
-    model.train()
-    for i, batch in enumerate(tqdm(train_loader, desc=f"spectra", position=0, leave=True)):
-
-        spectra, labels = tuple(t.to(device) for t in batch)
-        # spectra = spectra.to(device)
-        # labels = labels.to(device)
-
-        outputs = model(spectra)
-        loss = criterion(outputs, labels)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        allLoss.append(float(loss.cpu().detach().numpy()))
-    scheduler.step(loss)
-
-# %%
-# plt.plot(allLoss)
-
-# %%
-reportName = f'{experiment}BackgroundPx'
-torch.save(model.state_dict(), f'../models/{reportName}.pth')
->>>>>>> a53e49d3b58dd84318a69d8faef4882af582a868
