68c7f615abfda2efbb25b3252e17bf46f5149b81
Tyler Jost <tyler_jost@utexas.edu>
2023-01-11 10:30:41 -0600

    Looked at loss values

--- scripts/esam2/visualizations/plotROCESAM2.py
+++ scripts/esam2/visualizations/plotROCESAM2.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 # %%
-fileNames = ['esam2leaveOutCellROC.npy', 'esam2leaveOutScanROC.npy', 'esam2testWholeROC.npy']
+fileNames = ['esam2leaveOutCellROC', 'esam2leaveOutScanROC', 'esam2testWholeROC']
 labels = ['Leave Out Cell', 'Leave Out Scan', 'Shuffle Whole']
 resultsDir = '../../../results'
 
@@ -14,7 +14,7 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 
 for name, label in zip(fileNames, labels):
-    filePath = os.path.join(resultsDir, name)
+    filePath = os.path.join(resultsDir, name+'.npy')
     fpr, tpr, roc = np.load(filePath, allow_pickle=True)
     plt.plot(fpr, tpr,linewidth=3, label=label)
 
@@ -23,3 +23,10 @@ plt.title('esam2 ROC')
 plt.savefig('../../../figures/esam2ROC.png', dpi=600)
 plt.show()
 # %%
+lossNames = ['esam2LOScanDenoisedFinal', 'esam2WholeDenoisedFinal', 'esam2LOCellFinal']
+for fileName in lossNames:
+    filePath = os.path.join('../../../models', fileName+'.npy')
+    x = np.load(filePath, allow_pickle=True)
+    plt.plot(x, label=fileName)
+
+plt.legend()
